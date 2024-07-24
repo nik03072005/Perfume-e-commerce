12870bfec60902d990a8422b22ff053d6134c344
Nikhil Chandra <nc03072005@gmail.com>
2024-07-24 20:07:02 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById("hamburger");
+const list = document.getElementById("navlist");
+
+burger.addEventListener("click", ()=>{
+
+    list.classList.toggle("navlist-active");
+
+
+})
